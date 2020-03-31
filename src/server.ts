35b7f92da996fedeb0f48ed53f135120321b1837
George Goniotakis <georgios.goniotakis@outlook.com>
2020-03-31 21:06:50 +0100

    Upcoming welcome feature

--- src/server.ts
+++ src/server.ts
@@ -40,7 +40,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
     // Root Endpoint
     // Displays a simple message to the user
     app.get("/", async (req, res) => {
-        res.send("try GET /filteredimage?image_url={{}}")
+        res.send("Hello there! Try our new /filteredimage endpoint.")
     });
 
 
